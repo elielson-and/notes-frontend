11616e52018f68ca2ffa04fef89e479aad4ef8c3
Elielson <elielsonandre123@gmail.com>
2023-04-08 19:55:19 -0300

    feat: adding cssnano to dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    require('cssnano')({
+        preset: 'default',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
